49798366df4139a182c601926fd29b050afec1a5
assemblyad <61529304+assemblyad@users.noreply.github.com>
2022-04-02 18:21:41 -0600

    создадим пустой Python-скрипт. Внутри него создадим переменную json_text. Значение этой переменной должно быть таким, как указано тут: https://gist.github.com/KotovVitaliy/83e4eeabdd556431374dfc70d0ba9d37
    Наша задача с помощью библиотеки “json”, которую мы показывали на занятии, распарсить нашу переменную json_text и вывести текст второго сообщения с помощью функции print.

--- json_text_parsing.py
+++ json_text_parsing.py
@@ -3,7 +3,8 @@ import json
 string_as_json_foramt = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(string_as_json_foramt)
 obj1 =obj['messages']
-obj2 =obj1[1]
+obj2 =obj1[len(obj1)-1]
+
 
 key = "message"
 if key in obj2:
