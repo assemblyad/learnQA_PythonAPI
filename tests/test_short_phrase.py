ce0fb381f43f422d91572b9d9bb10e66bd6ada13
assemblyad <61529304+assemblyad@users.noreply.github.com>
2022-04-12 08:43:15 -0600

    Ex10: Тест на короткую фразу В рамках этой задачи с помощью pytest необходимо написать тест, который просит ввести в консоли любую фразу короче 15 символов. А затем с помощью assert проверяет, что фраза действительно короче 15 символов.
    Чтобы в переменную получить значение, введенное из консоли, необходимо написать вот такой код:
    
    phrase = input("Set a phrase: ")
    
    Внимание, чтобы pytest не игнорировал команду ввода с клавиатуры, запускать тест нужно с ключиком "-s": python -m pytest -s my_test.py
    
    =========================================
    
    Результатом должна стать ссылка на такой тест.директории tests/

---
+++ tests/test_short_phrase.py
@@ -0,0 +1,7 @@
+phrase = input("Set a phrase: ")
+print (len(phrase))
+
+class TestShortPhrase:
+    def test_phrase_len(self):
+        expected_len = 15
+        assert len(phrase) <= expected_len, f"The phrase length should be less than {expected_len}"
