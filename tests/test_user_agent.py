cc11b8eb694aa2b214388aa4d4c7f2a52754cc73
assemblyad <61529304+assemblyad@users.noreply.github.com>
2022-04-13 19:03:35 -0600

    Ex13: User Agent User Agent - это один из заголовков, позволяющий серверу узнавать, с какого девайса и браузера пришел запрос. Он формируется автоматически клиентом, например браузером. Определив, с какого девайса или браузера пришел к нам пользователь мы сможем отдать ему только тот контент, который ему нужен.
    Наш разработчик написал метод: https://playground.learnqa.ru/ajax/api/user_agent_check
    
    Метод определяет по строке заголовка User Agent следующие параметры:
    
    device - iOS или Android
    
    browser - Chrome, Firefox или другой браузер
    
    platform - мобильное приложение или веб
    
    Если метод не может определить какой-то из параметров, он выставляет значение Unknown.
    
    Наша задача написать параметризированный тест. Этот тест должен брать из дата-провайдера User Agent и ожидаемые значения, GET-делать запрос с этим User Agent и убеждаться, что результат работы нашего метода правильный - т.е. в ответе ожидаемое значение всех трех полей.
    
    Список User Agent и ожидаемых значений можно найти по этой ссылке: https://gist.github.com/KotovVitaliy/138894aa5b6fa442163561b5db6e2e26
    
    Пример того, как должен выглядеть запрос с указанным User Agent:
    
    requests.get(
    
        "https://playground.learnqa.ru/ajax/api/user_agent_check",
    
        headers={"User-Agent": "Some value here"}
    
    )
    
    ============================================================
    На самом деле метод не всегда работает правильно. Ответом к задаче должен быть список из тех User Agent, которые вернули неправильным хотя бы один параметр, с указанием того, какой именно параметр неправильный.

--- tests/test_user_agent.py
+++ tests/test_user_agent.py
@@ -1,30 +1,63 @@
-import pytest
 import requests
+import pytest
+import json
 
-class TestRequestUserAgent():
-    user_agent = [
-        ("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"),
-        ("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1"),
-        ("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"),
-        ("'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0'"),
-        ("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
-    ]
 
-    expected_result_dict = [
-        ("'platform': 'Mobile', 'browser': 'No', 'device': 'Android'"),
-        ("'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'"),
-        ("'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'"),
-        ("'platform': 'Web', 'browser': 'Chrome', 'device': 'No'"),
-        ("'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'")
-    ]
-
-    @pytest.mark.parametrize('user_agent', user_agent, 'expected_result_dict', expected_result_dict)
-    def test_header_validation(self, user_agent, expected_result_dict):
-        response = requests.get(
-            "https://playground.learnqa.ru/ajax/api/user_agent_check"
-            ,headers={"User-Agent": user_agent}
+class TestCheckUserAgent:
+    data = [
+        (
+            "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "Mobile",
+            "No",
+            "Android"
+        ),
+        (
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "Mobile",
+            "Chrome",
+            "iOS"
+        ),
+        (
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Googlebot",
+            "Unknown",
+            "Unknown"
+        ),
+        (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+            "Googlebot",
+            "Chrome",
+            "No"
+        ),
+        (
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+            "Mobile",
+            "No",
+            "iPhone"
         )
+    ]
 
+    @pytest.mark.parametrize('agent, platform, browser, device', data)
+    def test_check_agent(self, agent, platform, browser, device):
+        url = "https://playground.learnqa.ru/ajax/api/user_agent_check"
+        data = {"user-agent": agent}
+
+        response = requests.get(url, headers=data)
+        obj = json.loads(response.text)
+        """
+        print(response)
+        print(response.text)
+        print(obj)
+        """
+        response_browser_name = obj.get("browser")
+        response_device_name = obj.get("device")
+        response_platform_name = obj.get("platform")
+
+        assert response_browser_name == browser, f"The request with User Agent {agent} return incorrect browser name {response_browser_name}"
+        assert response_device_name == device, f"The request with User Agent {agent} return incorrect browser name {response_device_name}"
+        assert response_platform_name == platform, f"The browser name is unknown while expected name {response_platform_name}"
+
+        """
         print(response.json())
         print(response.request.headers)
         platform = response.json()['platform']
@@ -34,3 +67,4 @@ class TestRequestUserAgent():
         assert platform == expected_result_dict[platform], f"The platform ddoes not match"
         assert browser == expected_result_dict[browser], f"The browser does not match"
         assert device == expected_result_dict[device], f"The device does not match"
+        """
