d89b7a272fbdc54d66c84a27abc20cba9648e391
assemblyad <61529304+assemblyad@users.noreply.github.com>
2022-04-16 20:03:41 -0600

    Ex16: Запрос данных другого пользователя На занятиях в классе TestUserGet мы писали тест на запрос, показывающий данные пользователя. Мы покрыли тестами два кейса:
    - неавторизованный запрос на данные - там мы получили только username
    
    - авторизованный запрос - мы были авторизованы пользователем с ID 2 и делали запрос для получения данных того же пользователя, в этом случае мы получали все поля
    
    В этой задаче нужно написать тест, который авторизовывается одним пользователем, но получает данные другого (т.е. с другим ID). И убедиться, что в этом случае запрос также получает только username, так как мы не должны видеть остальные данные чужого пользователя.
    
    ===================================================
    Результатом работы должна быть ссылка на файл в вашем репозитории, где реализованы данные тесты.

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -41,12 +41,24 @@ class TestUserGet(BaseCase):
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
 
-        """
-        print(response.content)
-        print(response.text)
+    def test_get_user_details_with_wrong_user_id(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
 
-        Assertions.assert_json_has_key(response,"username")
-        Assertions.assert_json_has_not_key(response,"email")
-        Assertions.assert_json_has_not_key(response, "firstName")
-        Assertions.assert_json_has_not_key(response, "lastName")
+        response1 = MyRequest.post("/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
         """
+        url2 = f"https://playground.learnqa.ru/api/user/{user_id_from_auth_method}"
+        response2 = requests.get(url2, headers ={"x-csrf-token": token}, cookies ={"auth_sid":auth_sid})
+        """
+        response2 = MyRequest.get(f"/api/user/1", headers={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
